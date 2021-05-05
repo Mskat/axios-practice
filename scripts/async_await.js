15be8ec43fc146f7c4e6123db9cb46a6db03e385
Kat <zur.katarzyna@gmail.com>
2021-05-05 17:58:17 +0100

    Async-await example

---
+++ scripts/async_await.js
@@ -0,0 +1,16 @@
+const modAxios = require('axios');
+
+async function main() {
+    const strURL = 'https://jsonplaceholder.typicode.com/users/';
+    const objResult = await modAxios(
+        strURL,
+    );
+    const arrEmails = [];
+    const arrResults = objResult.data;
+    for (let i = 0; i < arrResults.length; i++) {
+        arrEmails.push(arrResults[i].email);
+    }
+    console.log(arrEmails);
+}
+
+main();
