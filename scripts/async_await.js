40438bd99aa09e1855a3f22fcdc3288c6d6eb0b2
Kat <zur.katarzyna@gmail.com>
2021-05-05 20:42:17 +0100

    Add try-catch

--- scripts/async_await.js
+++ scripts/async_await.js
@@ -2,15 +2,19 @@ const modAxios = require('axios');
 
 async function main() {
     const strURL = 'https://jsonplaceholder.typicode.com/users/';
-    const objResult = await modAxios(
-        strURL,
-    );
-    const arrEmails = [];
-    const arrResults = objResult.data;
-    for (let i = 0; i < arrResults.length; i++) {
-        arrEmails.push(arrResults[i].email);
+    try {
+        const objResult = await modAxios(
+            strURL,
+        );
+        const arrEmails = [];
+        const arrResults = objResult.data;
+        for (let i = 0; i < arrResults.length; i++) {
+            arrEmails.push(arrResults[i].email);
+        }
+        console.log(arrEmails);
+    } catch (err) {
+        console.log(err.message);
     }
-    console.log(arrEmails);
 }
 
 main();
